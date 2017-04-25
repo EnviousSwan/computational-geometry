c94162d0c5584d2b3051a507e4add160fbfe7765
Swan <swan@localhost.localdomain>
2017-04-25 15:08:48 +0300

    fix some geometry notation

--- src/main/scala/compgeom/geometry/Point.scala
+++ src/main/scala/compgeom/geometry/Point.scala
@@ -2,26 +2,26 @@ package compgeom.geometry
 
 case class Point(x: Double, y: Double) extends Ordered[Point] {
 
-	def distanceSquaredTo(other: Point): Double = {
-		val dx = x - other.x
-		val dy = y - other.y
-		dx * dx + dy * dy
-	}
-
 	def toTheLeft(line: Line): Boolean =
-		slope(line) < 0
+		crossProduct(line) < 0
 
 	def toTheRight(line: Line): Boolean =
-		slope(line) >  0
+		crossProduct(line) >  0
 
 	def onThe(line: Line): Boolean =
-		slope(line) == 0
+		crossProduct(line) == 0
 
-	private def slope(line: Line) = {
+	private def crossProduct(line: Line) = {
 		val Line(a, b) = line
 
 		(x - a.x) * (b.y - a.y) -
 			(y - a.y) * (b.x - a.x)
+	}
+
+	def distanceSquaredTo(other: Point): Double = {
+		val dx = x - other.x
+		val dy = y - other.y
+		dx * dx + dy * dy
 	}
 
 	def distanceTo(other: Point): Double =
@@ -32,17 +32,7 @@ case class Point(x: Double, y: Double) extends Ordered[Point] {
 
 		val Line(a, b) = line
 		val normal = sqrt(pow(b.x - a.x, 2) + pow(b.y - a.y, 2))
-		abs(slope(line)) / normal
-	}
-
-//	def draw(): Unit = StdDraw.point(x, y)
-
-	override def canEqual(that: Any): Boolean = that.isInstanceOf[Point]
-
-	override def equals(obj: scala.Any): Boolean = obj match {
-		case point: Point =>
-			point.canEqual(this) && x == point.x && y == point.y
-		case _ => false
+		abs(crossProduct(line)) / normal
 	}
 
 	override def compare(other: Point): Int =
--- src/main/scala/compgeom/geometry/Rectangle.scala
+++ src/main/scala/compgeom/geometry/Rectangle.scala
@@ -1,34 +1,34 @@
 package compgeom.geometry
 
-case class Rect(xmin: Double, ymin: Double,
-                xmax: Double, ymax: Double) {
+case class Rectangle(xmin: Double, ymin: Double,
+                     xmax: Double, ymax: Double) {
 
 	def contains(point: Point): Boolean =
 		point.x > xmin && point.x < xmax &&
 			point.y > ymin && point.y < ymax
 
-	def intersects(other: Rect): Boolean =
+	def intersects(other: Rectangle): Boolean =
 		xmax >= other.xmin && ymax >= other.ymin &&
 			xmin <= other.xmax && ymin <= other.ymax
 
 	def distanceSquaredTo(point: Point): Double = {
 		val dx = {
-			if      (point.x < xmin) point.x - xmin
+			if (point.x < xmin) point.x - xmin
 			else if (point.x > xmax) point.x - xmax
-			else     0.0
+			else 0.0
 		}
 		val dy = {
-			if      (point.y < ymin) point.y - ymin
+			if (point.y < ymin) point.y - ymin
 			else if (point.y > ymax) point.y - xmax
-			else     0.0
+			else 0.0
 		}
 
 		dx * dx + dy * dy
 	}
 
 	override def equals(obj: scala.Any): Boolean = obj match {
-		case rect: Rect =>
-				xmin == rect.xmin && ymin == rect.ymin &&
+		case rect: Rectangle =>
+			xmin == rect.xmin && ymin == rect.ymin &&
 				xmax == rect.xmax && ymax == rect.ymax
 		case _ => false
 	}
